4a475ea8ef2f7a0624977309d67907f6de0b3d28
anel27 <39824825+anel27@users.noreply.github.com>
2018-06-02 18:25:23 +0200

    you block out

---
+++ vi_ste_blokirani.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+
+	<style>
+	body {
+    display: block;
+    margin: 0px;
+	background: dimgrey;
+}
+.bod {
+    
+    text-align: center;
+    margin-top: 100px;
+    color: #62cbff;
+    font-size: 32px;
+    list-style-type: none;
+}
+.bod li a {
+    color: #faf2ff;
+}
+	</style>
+</head>
+<body>
+<div class = "bod">
+<div class = "mu23">VI STE BLOKIRANI NA NASEM SAJTU !!!</div>
+<div class = "mu24">ZA VISE INFORMACIJA OBRATITE SE NA E-MAIL :</div>
+<li><a href="kontakt.php"><span class="4kontakt"></span>kontakt@knjigomanija.com</a></li>
+</div>
+
+
+
+
+</body>
+
+</html>
