088f5a6bc23fba185840a50bb9f30b23da7ea752
anel27 <39824825+anel27@users.noreply.github.com>
2018-06-02 18:20:28 +0200

    orderder book

---
+++ naruci.php
@@ -0,0 +1,91 @@
+<?php
+	// the shopping cart needs sessions, to start one
+	/*
+		Array of session(
+			cart => array (
+				book_isbn (get from $_GET['book_isbn']) => number of books
+			),
+			items => 0,
+			total_price => '0.00'
+		)
+	*/
+	session_start();
+	require_once "./functions/database_functions.php";
+	// print out header here
+	$title = "Narudzbina";
+	require "./template/header.php";
+
+	if(isset($_SESSION['cart']) && (array_count_values($_SESSION['cart']))){
+?>
+	<table class="table">
+		<tr>
+			<th>Knjige</th>
+	   			<th>Cijena</th>
+	  			<th>Kolicina</th>
+	   			<th>Ukupno</th>
+	    </tr>
+	    	<?php
+			    foreach($_SESSION['cart'] as $isbn => $qty){
+					$conn = db_connect();
+					$book = mysqli_fetch_assoc(getBookByIsbn($conn, $isbn));
+			?>
+		<tr>
+			<td><?php echo $book['book_title'] . " by " . $book['book_author']; ?></td>
+			<td><?php echo "$" . $book['book_price']; ?></td>
+			<td><?php echo $qty; ?></td>
+			<td><?php echo "$" . $qty * $book['book_price']; ?></td>
+		</tr>
+		<?php } ?>
+		<tr>
+			<th>&nbsp;</th>
+			<th>&nbsp;</th>
+			<th><?php echo $_SESSION['total_items']; ?></th>
+			<th><?php echo "$" . $_SESSION['total_price']; ?></th>
+		</tr>
+	</table>
+	<form method="post" action="hvala.php" class="form-horizontal">
+		<?php if(isset($_SESSION['err']) && $_SESSION['err'] == 1){ ?>
+			<p class="text-danger">Sva polja moraju biti popunjena</p>
+			<?php } ?>
+		<div class="form-group">
+			<label for="name" class="control-label col-md-4">Ime:</label>
+			<div class="col-md-4">
+				<input type="text" name="name" class="col-md-4" class="form-control">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="address" class="control-label col-md-4">Adresa:</label>
+			<div class="col-md-4">
+				<input type="text" name="address" class="col-md-4" class="form-control">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="city" class="control-label col-md-4">Grad:</label>
+			<div class="col-md-4">
+				<input type="text" name="city" class="col-md-4" class="form-control">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="zip_code" class="control-label col-md-4">Broj Poste:</label>
+			<div class="col-md-4">
+				<input type="text" name="zip_code" class="col-md-4" class="form-control">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="country" class="control-label col-md-4">Drzava:</label>
+			<div class="col-md-4">
+				<input type="text" name="country" class="col-md-4" class="form-control">
+			</div>
+		</div>
+		<div class="form-group">
+			<input type="submit" name="submit" value="Naruci" class="btn btn-primary">
+		</div>
+	</form>
+	<p class="lead">Kliknite na NARUCI da potvrdite vasu narudzbinu.</p>
+<?php
+	} else {
+		echo "<p class=\"text-warning\">Your cart is empty! Please make sure you add some books in it!</p>";
+	}
+	if(isset($conn)){ mysqli_close($conn); }
+	require_once "./template/footer.php";
+?>
