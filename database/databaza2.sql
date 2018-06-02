eed98002f93f826de217a9f69f69b9d693a04c74
anel27 <39824825+anel27@users.noreply.github.com>
2018-06-02 18:04:40 +0200

    databasa please read com

---
+++ database/databaza2.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 23, 2018 at 12:16 PM
+
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `www_project`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `name` varchar(20) COLLATE latin1_general_ci NOT NULL,
+  `pass` varchar(40) COLLATE latin1_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`name`, `pass`) VALUES
+('admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `book_isbn` varchar(20) COLLATE latin1_general_ci NOT NULL,
+  `book_title` varchar(60) COLLATE latin1_general_ci DEFAULT NULL,
+  `book_author` varchar(60) COLLATE latin1_general_ci DEFAULT NULL,
+  `book_image` varchar(40) COLLATE latin1_general_ci DEFAULT NULL,
+  `book_descr` text COLLATE latin1_general_ci,
+  `book_price` decimal(6,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`book_isbn`, `book_title`, `book_author`, `book_image`, `book_descr`, `book_price`) VALUES
+('1', 'O tajni i tajnom druÅ¡tvu', 'Georg Zimel', 'O-tajni-i-tajnom-drustvu-61833.jpg', 'Tajna u ovom smislu â€“ skrivanje neke stvarnosti pozitivnim ili negativnim sredstvima â€“ jedna\r\nje od najveÄ‡ih tekovina ÄovjeÄanstva. U suprosti spram djeÄje naivnosti u kojoj se svaka\r\npredstava odmah izgovara, u kojoj je svaki pothvat izloÅ¾en pogledima, tajna u Å¾ivot ljudi\r\nunosi ogromno proÅ¡irenje, jer raznovrsni sadrÅ¾aji unatoÄ punom publicitetu jednostavno\r\nmoraju ostati skriveni. Tajna otvara moguÄ‡nost jednog, takoreÄ‡i, drugog svijeta pored onog\r\nkoji je pred nama, a uticaj tog drugog svijeta na ovaj prvi je izvanredno snaÅ¾an. Svaki se\r\nodnos izmeÄ‘u dva Äovjeka, izmeÄ‘u dvije grupe ljudi karakteriÅ¡e time da li se i koliko tajne u\r\nnjemu skriva. ÄŒak i tamo gdje Äovjek ne primjeÄ‡uje postojanje tajne time mijenja ponaÅ¡anje\r\nprikrivaÄa, i samim tim modifikuje cijeli odnos... (Georg Zimel)', '25.00'),
+('45', 'sdf', 'djura palica', 'wh.png', 'dfgdfgdfg', '34.00'),
+('453', 'wheel of fortune The Javascript Game!', 'Georg Zimel', 'dolarska-zaoka.jpg', 'werwerwer', '34.00'),
+('978-0-321-94786-4', 'Learning', 'Jakob Iversen, Michael Eierman', 'mobile_app.jpg', 'Now, one book ca', '20.00'),
+('978-0-7303-1484-4', 'Doing Good By Doing Good', 'Peter Baines', 'doing_good.jpg', 'Doing Good by Doing Good shows companies how to improve the bottom line by implementing an engaging, authentic, and business-enhancing program that helps staff and business thrive. International CSR consultant Peter Baines draws upon lessons learnt from the challenges faced in his career as a police officer, forensic investigator, and founder of Hands Across the Water to describe the Australian CSR landscape, and the factors that make up a program that benefits everyone involved. Case studies illustrate the real effect of CSR on both business and society, with clear guidance toward maximizing involvement, engaging all employees, and improving the bottom line. The case studies draw out the companies that are focusing on creating shared value in meeting the challenges of society whilst at the same time bringing strong economic returns.\r\n\r\nConsumers are now expecting that big businesses with ever-increasing profits give back to the community from which those profits arise. At the same time, shareholders are demanding their share and are happy to see dividends soar. Getting this right is a balancing act, and Doing Good by Doing Good helps companies delineate a plan of action for getting it done.', '20.00'),
+('978-1-118-94924-5', 'Programmable Logic Controllers', 'Dag H. Hanssen', 'logic_program.jpg', 'Widely used across industrial and manufacturing automation, Programmable Logic Controllers (PLCs) perform a broad range of electromechanical tasks with multiple input and output arrangements, designed specifically to cope in severe environmental conditions such as automotive and chemical plants.Programmable Logic Controllers: A Practical Approach using CoDeSys is a hands-on guide to rapidly gain proficiency in the development and operation of PLCs based on the IEC 61131-3 standard. Using the freely-available* software tool CoDeSys, which is widely used in industrial design automation projects, the author takes a highly practical approach to PLC design using real-world examples. The design tool, CoDeSys, also features a built in simulator / soft PLC enabling the reader to undertake exercises and test the examples.', '20.00'),
+('978-1-1180-2669-4', 'Professional JavaScript for Web Developers, 3rd Edition', 'Nicholas C. Zakas', 'pro_js.jpg', 'If you want to achieve JavaScript\'s full potential, it is critical to understand its nature, history, and limitations. To that end, this updated version of the bestseller by veteran author and JavaScript guru Nicholas C. Zakas covers JavaScript from its very beginning to the present-day incarnations including the DOM, Ajax, and HTML5. Zakas shows you how to extend this powerful language to meet specific needs and create dynamic user interfaces for the web that blur the line between desktop and internet. By the end of the book, you\'ll have a strong understanding of the significant advances in web development as they relate to JavaScript so that you can apply them to your next website.', '20.00'),
+('978-1-44937-019-0', 'Learning Web App Development', 'Semmy Purewal', 'web_app_dev.jpg', 'Grasp the fundamentals of web application development by building a simple database-backed app from scratch, using HTML, JavaScript, and other open source tools. Through hands-on tutorials, this practical guide shows inexperienced web app developers how to create a user interface, write a server, build client-server communication, and use a cloud-based service to deploy the application.\r\n\r\nEach chapter includes practice problems, full examples, and mental models of the development workflow. Ideal for a college-level course, this book helps you get started with web app development by providing you with a solid grounding in the process.', '20.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customerid` int(10) UNSIGNED NOT NULL,
+  `name` varchar(60) COLLATE latin1_general_ci NOT NULL,
+  `address` varchar(80) COLLATE latin1_general_ci NOT NULL,
+  `city` varchar(30) COLLATE latin1_general_ci NOT NULL,
+  `zip_code` varchar(10) COLLATE latin1_general_ci NOT NULL,
+  `country` varchar(60) COLLATE latin1_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`customerid`, `name`, `address`, `city`, `zip_code`, `country`) VALUES
+(1, 'a', 'a', 'a', 'a', 'a'),
+(2, 'b', 'b', 'b', 'b', 'b'),
+(3, 'test', '123 test', '12121', 'test', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderid` int(10) UNSIGNED NOT NULL,
+  `customerid` int(10) UNSIGNED NOT NULL,
+  `amount` decimal(6,2) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ship_name` char(60) COLLATE latin1_general_ci NOT NULL,
+  `ship_address` char(80) COLLATE latin1_general_ci NOT NULL,
+  `ship_city` char(30) COLLATE latin1_general_ci NOT NULL,
+  `ship_zip_code` char(10) COLLATE latin1_general_ci NOT NULL,
+  `ship_country` char(20) COLLATE latin1_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`orderid`, `customerid`, `amount`, `date`, `ship_name`, `ship_address`, `ship_city`, `ship_zip_code`, `ship_country`) VALUES
+(1, 1, '60.00', '2015-12-03 13:30:12', 'a', 'a', 'a', 'a', 'a'),
+(2, 2, '60.00', '2015-12-03 13:31:12', 'b', 'b', 'b', 'b', 'b'),
+(3, 3, '20.00', '2015-12-03 19:34:21', 'test', '123 test', '12121', 'test', 'test'),
+(4, 1, '20.00', '2015-12-04 10:19:14', 'a', 'a', 'a', 'a', 'a');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `orderid` int(10) UNSIGNED NOT NULL,
+  `book_isbn` varchar(20) COLLATE latin1_general_ci NOT NULL,
+  `item_price` decimal(6,2) NOT NULL,
+  `quantity` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`orderid`, `book_isbn`, `item_price`, `quantity`) VALUES
+(1, '978-1-118-94924-5', '20.00', 1),
+(1, '978-1-44937-019-0', '20.00', 1),
+(1, '978-1-49192-706-9', '20.00', 1),
+(2, '978-1-118-94924-5', '20.00', 1),
+(2, '978-1-44937-019-0', '20.00', 1),
+(2, '978-1-49192-706-9', '20.00', 1),
+(3, '978-0-321-94786-4', '20.00', 1),
+(1, '978-1-49192-706-9', '20.00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `blokiraj` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `trn_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `blokiraj`, `username`, `email`, `password`, `trn_date`) VALUES
+(4, 0, 'admin', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '2018-05-19 23:19:02'),
+(8, 1, 'mile', 'mile@gmail.com', 'ea08e678edbf8892b8d67fc36f4a3bf9', '2018-05-21 23:51:24'),
+(14, 0, 'zumi1', 'zumi1@zumi.com', 'b021668bdcb005bebafe56a0fda860d9', '2018-05-23 00:22:58'),
+(15, 1, 'liki', 'liki@gmail.com', '251319fee3698c9c3322ff75d8a6597a', '2018-05-23 00:24:10'),
+(16, 1, 'test', 'asdasd@gmail.com', '098f6bcd4621d373cade4e832627b4f6', '2018-05-23 02:02:40');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`name`,`pass`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_isbn`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customerid`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customerid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
