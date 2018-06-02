970d9194346b2cc7c6e20aea9f8843fab680e488
anel27 <39824825+anel27@users.noreply.github.com>
2018-06-02 18:24:21 +0200

    registartion people

---
+++ registracija.php
@@ -0,0 +1,45 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Registracija</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<?php
+	require('db.php');
+    // If form submitted, insert values into the database.
+    if (isset($_REQUEST['username'])){
+		$username = stripslashes($_REQUEST['username']); // removes backslashes
+		$username = mysqli_real_escape_string($con,$username); //escapes special characters in a string
+		$email = stripslashes($_REQUEST['email']);
+		$email = mysqli_real_escape_string($con,$email);
+		$password = stripslashes($_REQUEST['password']);
+		$password = mysqli_real_escape_string($con,$password);
+
+		$trn_date = date("Y-m-d H:i:s");
+        $query = "INSERT into `users` (username, password, email, trn_date) VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
+        $result = mysqli_query($con,$query);
+        if($result){
+           header("Location: prijava.php");
+        }
+    }else{
+?>
+<div class="form">
+<h1>Registracija</h1>
+<form name="registration" action="" method="post">
+<input type="text" name="username" placeholder="Username" required />
+<input type="email" name="email" placeholder="Email" required />
+<input type="password" name="password" placeholder="Password" required />
+<input type="submit" name="submit" value="Registruj me" />
+</form>
+<br /><br />
+
+</div>
+<?php } ?>
+</body>
+</html>
