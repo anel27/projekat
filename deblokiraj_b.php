f5720bdcac167c3c852952000b56c39176c8856d
anel27 <39824825+anel27@users.noreply.github.com>
2018-06-02 18:14:56 +0200

    unblocking

---
+++ deblokiraj_b.php
@@ -0,0 +1,25 @@
+ <?php
+include('db.php');
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+//
+
+//
+$b56 = $_GET['id'];
+
+
+                 $sql = "UPDATE `users` ". "SET `blokiraj` = '0' ". 
+               "WHERE `id` = '$b56'" ;
+                 
+
+			   if (mysqli_query($con, $sql)) {
+      echo "Uspjesno smo blokirali korisnika";
+	  header("Location: admin_izmjena_korisnika.php");
+   } else {
+      echo "Nismo uspjeli da blokiramo korisnika,mozda korisnik ne postoji: " . mysqli_error($con);
+   }
+
+?>
